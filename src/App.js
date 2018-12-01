3f6636e6a4d751c758a967406f40164c0cf903d5
Davit Barbakadze <jayarjo@gmail.com>
2018-12-01 16:52:58 +0400

    add slowbug

--- src/App.js
+++ src/App.js
@@ -3,17 +3,20 @@ import React, { lazy, Suspense, Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-// const MaterialIcon = ({ icon }) => {
-//     const Icon = lazy(() => import(`@material-ui/icons/${icon}`))
-//     return <Suspense fallback={<div />}><Icon /></Suspense>;
-// }
+// ????
+// the code below cause build process to hangout for 30 secs, sometimes resulting in the error
+// (FATAL ERROR: Ineffective mark-compacts near heap limit Allocation failed - JavaScript heap out of memory)
+const MaterialIcon = ({ icon }) => {
+    const Icon = lazy(() => import(`@material-ui/icons/${icon}`))
+    return <Suspense fallback={<div />}><Icon /></Suspense>;
+}
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          {/* <MaterialIcon icon="Dashboard" /> */}
+          <MaterialIcon icon="Dashboard" />
           <img src={logo} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
