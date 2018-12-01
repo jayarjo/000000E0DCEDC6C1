fd8f2fa47c46420f357ccc7eeeed78d524771d84
Davit Barbakadze <jayarjo@gmail.com>
2018-12-01 16:51:03 +0400

    init

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
-import React, { Component } from 'react';
+import React, { lazy, Suspense, Component } from 'react';
+
 import logo from './logo.svg';
 import './App.css';
+
+// const MaterialIcon = ({ icon }) => {
+//     const Icon = lazy(() => import(`@material-ui/icons/${icon}`))
+//     return <Suspense fallback={<div />}><Icon /></Suspense>;
+// }
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
+          {/* <MaterialIcon icon="Dashboard" /> */}
           <img src={logo} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
